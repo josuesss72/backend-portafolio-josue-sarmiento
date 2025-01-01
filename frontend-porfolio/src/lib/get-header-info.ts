1af20538489461cc65bcad1587cfbdb9cb25d1d7
josuesss72 <jjdanielsarmiento@gmail.com>
2025-01-01 14:23:34 -0500

    delete: se elimino la carpeta frontend

--- frontend-porfolio/src/lib/get-header-info.ts
+++
@@ -1,9 +0,0 @@
-import { query } from "./strapi";
-import { IHeader } from "@/types/header-type";
-import { IStrapiResponse } from "@/types/strapi-type";
-
-export function getHeaderInfo() {
-  return query("header?populate=*").then((res: IStrapiResponse<IHeader>) => {
-    return res;
-  });
-}
--- frontend-porfolio/src/lib/strapi.ts
+++
@@ -1,9 +0,0 @@
-export const { STRAPI_HOST, STRAPI_TOKEN, NEXT_PUBLIC_STRAPI_HOST } = process.env;
-
-export async function query(url: string) {
-  return await fetch(`${STRAPI_HOST}/api/${url}`, {
-    headers: {
-      Authorization: `Bearer ${STRAPI_TOKEN}`,
-    },
-  }).then(res => res.json())
-}
